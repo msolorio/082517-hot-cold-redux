81c001ba49054ae11eea43c87463336b28f764a4
msolorio <msolorio789@gmail.com>
2017-08-20 16:58:16 -0700

    add CI with Travis CI to deploy to netlify

--- src/components/game.test.js
+++ src/components/game.test.js
@@ -10,14 +10,4 @@ describe('<Game />', () => {
   it('Renders without crashing', () => {
       shallow(<Game />);
   });
-
-
-
-  // it('guesses the correct answer', () => {
-  //   const gameComponent = mount(<Game />);
-  //   const guessCount = gameComponent.find(GuessCount);
-  //   console.log(gameComponent.instance().state.correctAnswer);
-  //   gameComponent.instance().setState({guesses: [12]});
-  //   expect(guessCount.props().count).equal(1);
-  // });
 });
